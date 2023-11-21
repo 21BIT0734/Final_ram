5179a497d1465df5d9bba1d48cc1b47e7fb0fa95
21BIT0734 <144110480+21BIT0734@users.noreply.github.com>
2023-11-21 21:23:22 +0530

    Update Cart.jsx

--- frontend/src/Cart.jsx
+++ frontend/src/Cart.jsx
@@ -28,7 +28,7 @@ const Cart = () => {
     console.log('Updating database with new quantities:', orderData);
 
     // For demonstration purposes, let's assume you have a backend endpoint for placing orders
-    fetch('http://localhost:4000/place-order', {
+    fetch('https://final-ram-69.vercel.app/place-order', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
