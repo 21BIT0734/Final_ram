de1559ca43ca364b87375e0fe7a917dbe4dd83ae
21BIT0734 <144110480+21BIT0734@users.noreply.github.com>
2023-11-21 12:18:11 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './routers/router.jsx'
+import router from './routers/Router.jsx'
 import { CartProvider } from './CartContext.jsx'
 
 
