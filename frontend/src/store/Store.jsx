9b2812600eb10dfa86b4b24424371eacf27bc47b
21BIT0734 <144110480+21BIT0734@users.noreply.github.com>
2023-11-21 13:14:21 +0530

    Update Store.jsx

--- frontend/src/store/Store.jsx
+++ frontend/src/store/Store.jsx
@@ -25,7 +25,7 @@ const Store = () => {
 
   useEffect(()=>
   {
-    fetch("http://localhost:4000/view-product").then(res => res.json()).then(data => setGadgets(data));
+    fetch("https://final-ram-69.vercel.app/view-product").then(res => res.json()).then(data => setGadgets(data));
   },[])
   return (
     <div className='mt-28 px-4 lg:px24'>
