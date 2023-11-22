1f58e45376e0a85967128bbcae3cfc71261f725a
21BIT0734 <144110480+21BIT0734@users.noreply.github.com>
2023-11-22 15:09:51 +0530

    Update UDashboard.jsx

--- frontend/src/UDashboard/UDashboard.jsx
+++ frontend/src/UDashboard/UDashboard.jsx
@@ -5,6 +5,18 @@ const UDashboard = () => {
   return (
     <div>
       UserDashboard
+      <p>
+      IT A ROLE BASED AUTHENTICATION
+      THIS IS USER DASHBOARD
+
+      IF YOU WANT TO ENTER INTO ADMIN DAHBOARD CONTACT DEVELOPER TEAM 187
+      FOR 
+      Gmail:s69
+      Password:s69
+
+      Admin Access Given
+      </p>
+
       
     </div>
   )
